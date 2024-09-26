0fc7e59133af699d6ef3aa9fcc5304104f998fbb
José Corrochano Pardo <118207818+Josecp03@users.noreply.github.com>
2024-09-26 12:54:13 +0200

    Repasando para el primer examen de AD

---
+++ ACCESO_A_DATOS/TEMA-1/eclipse-workspace-tema1/PruebasExamen/src/tema1/pruebasFile.java
@@ -0,0 +1,29 @@
+package tema1;
+
+import java.io.*;
+
+public class pruebasFile {
+	public static void main(String[] args) {
+		
+		// Inicializar el fichero en el directorio actual
+		String dir = "."; // El "." indica el directorio actual
+		File f = new File(dir);
+		
+		// Guardar los archivos en un array
+		String[] archivos = f.list();
+		
+		// Imprimir el número de archivos que hay
+		System.out.printf("Ficheros en el directorio actual: %d %n", archivos.length);
+		
+		// Recorrer todos los archivos del directorio actual
+		for (int i = 0; i < archivos.length; i++) {
+			
+			// Creo un nuevo objeto file para almacenar el archivo actual dentro del directorio que ya hemo sinicializado antes
+			File f2 = new File(f, archivos[i]);
+			
+			// Imprimir por pantalla si el archivo actual es archivo o directorio
+			System.out.printf("Nombre: %s, es fichero?: %b, es directorio?: %b %n", archivos[i], f2.isFile(), f2.isDirectory());
+			
+		}
+	}
+}
