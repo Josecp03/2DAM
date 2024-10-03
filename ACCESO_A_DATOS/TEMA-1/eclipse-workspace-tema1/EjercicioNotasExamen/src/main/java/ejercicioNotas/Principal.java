7c025d00329cc1ad9cf618d4ca41633c7d7245c7
José Corrochano Pardo <118207818+Josecp03@users.noreply.github.com>
2024-10-03 12:13:49 +0200

    Ejercicio NotasAlumnos y Ejercicio leer XML AD

--- ACCESO_A_DATOS/TEMA-1/eclipse-workspace-tema1/EjercicioNotasExamen/src/main/java/ejercicioNotas/Principal.java
+++ ACCESO_A_DATOS/TEMA-1/eclipse-workspace-tema1/EjercicioNotasExamen/src/main/java/ejercicioNotas/Principal.java
@@ -1,9 +1,17 @@
 package ejercicioNotas;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import xmlAlumnos.Nota;
 
 public class Principal {
 
@@ -38,7 +46,7 @@ public class Principal {
 				actualizarAlumnos();
 				break;
 			case 4:
-
+				crearXML();
 				break;
 			case 5:
 				System.out.println("FIN DEL MENÚ!");
@@ -55,6 +63,172 @@ public class Principal {
 
 	}
 
+	private static void crearXML() throws IOException {
+		
+		// Inicializar variables
+		int numalumno; 
+		String nombre; 
+		String localidad; 
+		int numasig;
+		float notamedia;
+
+		// Cargar el fichero productos en la lista de alumnos
+		ArrayList<xmlAlumnos.Alumno> listaAlumnos = new ArrayList<>();
+		
+		// Inicializar el objeto file
+		File fichero = new File(".\\Alumnos.dat");
+		
+		try {
+			
+			// Declarar el fichero de acceso aleatorio
+			RandomAccessFile file = new RandomAccessFile(fichero, "r");
+
+			// Inicializar la posición a 0
+			int posicion = 0; 
+			
+			// Recorrer el fichero
+			for (;;) { 
+				
+				// Posicionarse correctamente
+				file.seek(posicion);
+				
+				// Leer el número de alumno
+				numalumno = file.readInt();
+				
+				// Obtener los datos si no es vacío
+				if (numalumno != 0) {
+					
+					// Leer el nombre
+					nombre = "";
+					for (int i = 0; i < 20; i++) {
+						nombre = nombre + file.readChar();
+					}
+					
+					// Leer la localidad
+					localidad = "";
+					for (int i = 0; i < 20; i++) {
+						localidad = localidad + file.readChar();
+					}
+					
+					// Leer el número de asignaturas
+					numasig = file.readInt();
+					
+					// Leer la nota media
+					notamedia = file.readFloat();
+					
+					// Inicializar la lista de notas
+					ArrayList<xmlAlumnos.Nota> notas = listaNotas(numalumno);
+					
+					// Crear la lista y añadirla
+					xmlAlumnos.Alumno alumno = new xmlAlumnos.Alumno(numalumno, nombre.trim(), localidad.trim(), numasig, notamedia, notas);
+					listaAlumnos.add(alumno);
+
+				}
+				
+				// Posicionarnos en el siguiente alumno
+				posicion = posicion + LON_Alumnos;
+
+				// Salir del bucle cuando lleguemos al final del fichero
+				if (posicion >= file.length()) {
+					break;
+				}
+					
+			} 
+
+			// Cerrar el fichero
+			file.close();
+			
+			// GENERAR EL XML, creamos un objeto de la raiz, y asignamos los productos
+			xmlAlumnos.ListaAlumnos aa = new xmlAlumnos.ListaAlumnos();
+			aa.setListaAlumnos(listaAlumnos);;
+			
+			// Inicializar el contexto
+			JAXBContext context;
+			try {
+				
+				context = JAXBContext.newInstance(xmlAlumnos.ListaAlumnos.class);
+				Marshaller m = context.createMarshaller();
+				m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+				m.marshal(aa, System.out);
+				m.marshal(aa, new File(".\\ProductosVentas.xml"));
+			} catch (JAXBException e) {
+				e.printStackTrace();
+			}
+
+		} catch (FileNotFoundException e) {
+			
+			System.out.println("**** FICHERO NO ENCONTRADO ****");
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static ArrayList<Nota> listaNotas(int numalumno) throws IOException {
+		
+		// Inicializar el Arrayist
+		ArrayList<xmlAlumnos.Nota> notas = new ArrayList<xmlAlumnos.Nota>();
+		
+		// Inicializar los objetos File
+		File fichero = new File(".\\Notas.dat");
+		
+		// Declarar el fichero de acceso aleatorio
+		RandomAccessFile file = new RandomAccessFile(fichero, "r");
+		
+		// Inicializar la posicion
+		int posicion = 0;
+		
+		// Inicializar variables
+		int numAlumnoActual;
+		String asignatura = "";
+		float nota = 0;
+		
+		// Recorrer el fichero
+		for(;;) {
+			
+			// Posicionarnos correctamente
+			file.seek(posicion);
+			
+			// Leer el número de alumno actual
+			numAlumnoActual = file.readInt();
+			
+			// Leer la asignatura
+			asignatura = "";
+			for (int i = 0; i < 20; i++) {
+				asignatura += file.readChar();
+			}
+						
+			// Comprobar que sea igual al que le he pasado como parámetro
+			if (numAlumnoActual == numalumno) {
+				
+				// Leer la nota
+				nota = file.readFloat();
+				
+				// Crear la nota
+				xmlAlumnos.Nota n = new xmlAlumnos.Nota(asignatura.trim(), nota);
+				
+				// Añadir la nota a la lista
+				notas.add(n);
+				
+			}
+
+			// Actualizar la posición
+			posicion += LON_Notas;
+			
+			// Salirnos del fichero cuando llegue al final
+			if (posicion >= file.length()) {
+				break;
+			}
+		
+		}
+		
+		// Cerrar el fichero
+		file.close();
+		
+		// Devolver la lista de ventas
+		return notas;
+		
+	}
+
 	// Método para actualizar las nota sy el número de asinaturas de cada alumno
 	private static void actualizarAlumnos() throws IOException {
 
@@ -66,11 +240,14 @@ public class Principal {
 
 		// Inicializar variables
 		int numAlumno;
-		char nombreAlumno[] = new char[20];
-		char localidad[] = new char[20];
-		char aux;
 		int numAsignaturas;
 		float nota;
+		float sumaMedias = 0;
+		int contadorAlumnos = 0;
+		float notaMaxima = 0;
+		char nombreAlumno[] = new char[20];
+		char aux;
+		String nombreAlumnoNotaMaxima = "";
 
 		// Establecer la posición a 0 para que empiece desde el principio
 		int posicion = 0;
@@ -86,28 +263,133 @@ public class Principal {
 
 			if (numAlumno != 0) {
 
+				// Aumentar el contador de alumnos
+				contadorAlumnos ++;
+				
 				// Calcular el número de asignaturas con la llamada al método
 				numAsignaturas = calcularNumeroAsignaturas(numAlumno);
 				
-				// Obtener los datos restantes
-				numAsignaturas = file.readInt();
-				nota = file.readFloat();
+				// Calcular la nota media de cada alumno
+				nota = calcularNotaMedia(numAlumno, numAsignaturas);
 
-				// Imprimir el registro actual
-				System.out.printf("%7s %-20s %-20s %9s %11s %n", numAlumno, nombreS, localidadS, numAsignaturas, nota);
+				// Calcular el nombre del alumno con nota media máxima
+				if (nota > notaMaxima) {
+					notaMaxima = nota;
+					
+					// Leer el nombre del alumno con la nota máxima actual
+					// Obtener el nombre del alumno recorriendo uno a uno los caracteres
+					for (int i = 0; i < nombreAlumno.length; i++) {
+						aux = file.readChar(); // Leer el caracter
+						nombreAlumno[i] = aux; // Guardarlo en el array formando el nombre completo
+					}
 
+					// Convertir el array en un string
+					String nombreAlumnoS = new String(nombreAlumno);
+					
+					// Asignar ese nombre al nombre de la máxima media actual
+					nombreAlumnoNotaMaxima = nombreAlumnoS;
+					
+				}
+				
+				// Sumar todas las notasMedias de todos los alumnos
+				sumaMedias += nota;
+				
+				// Posicionarnos donde el número de asignaturas
+				file.seek(posicion + 84);
+				
+				// Escribir el número de asignaturas de ese alumno
+				file.writeInt(numAsignaturas);
+				
+				// Actualizar de nuevo la posición para colocarnos donde la nota media
+				file.seek(posicion + 88);
+				
+				// Escribir la nota media del alumno
+				file.writeFloat(nota);
+				
 			}
 
 			// Posicionarse en el siguiente alumno
-			posicion = posicion + LON_Alumnos;
+			posicion += LON_Alumnos;
 
 			// Salir del for cuando haya recorrido todos los bytes (Y cerrar el file)
-			if (file.getFilePointer() == file.length()) {
-				file.close();
+			if (posicion >= file.length()) {
+				
+				// Imprimir el fichero actualizado
+				listarAlumnos();
+				System.out.printf("%7s %-20s %-20s %9s %11s %n", "-------", "--------------------", "--------------------",
+						"-------", "-----------");
+				
+				// Imprimir el nombre del alumno con mayor nota media
+				System.out.println("Alumno con nota media máxima: " + nombreAlumnoNotaMaxima);
+				
+				// Imprimir la media de nota total
+				System.out.printf("Media de nota total: %.2f %n", sumaMedias/contadorAlumnos);
 				break;
 			}
 
 		}
+		
+		// Cerrar el fichero
+		file.close();
+		
+	}
+
+	private static float calcularNotaMedia(int numAlumnoBuscado, int numAsignaturas) throws IOException{
+		
+		// Inicializar el objeto File
+		File fichero = new File(".\\Notas.dat");
+
+		// Declarar el fichero de acceso aleatorio
+		RandomAccessFile file = new RandomAccessFile(fichero, "r");
+
+		// Inicializar variables
+		int numAlumnoActual;
+		float nota;
+
+		// Establecer la posición a 0 para que empiece desde el principio
+		int posicion = 0;
+
+		// Inicializar la suma de las notas
+		float sumaNotas = 0;
+		
+		// Recorrer el fichero
+		for (;;) {
+
+			// Situarse en la posición inicializada
+			file.seek(posicion);
+
+			// Obtener el código del producto
+			numAlumnoActual = file.readInt();
+
+			// Comprobar que sea el alumno buscado
+			if (numAlumnoActual == numAlumnoBuscado) {
+				
+				// Posicionarnos en el registro de la nota
+				file.seek(posicion + 44);
+				
+				// Obtener la nota
+				nota = file.readFloat();
+				
+				// Actualizar la suma de las notas
+				sumaNotas += nota;
+				
+			}
+
+			// Posicionarse en el siguiente alumno
+			posicion = posicion + LON_Notas;
+
+			// Salir del for cuando haya recorrido todos los bytes (Y cerrar el file)
+			if (posicion >= file.length()) {
+				break;
+			}
+
+		}
+		
+		// Cerrar el fichero
+		file.close();
+		
+		// Devolver la media de las notas
+		return sumaNotas/numAsignaturas;
 		
 	}
 
@@ -122,9 +404,6 @@ public class Principal {
 
 		// Inicializar variables
 		int numAlumnoActual;
-		char nombreAsignatura[] = new char[20];
-		char aux;
-		float nota;
 
 		// Establecer la posición a 0 para que empiece desde el principio
 		int posicion = 0;
@@ -149,25 +428,20 @@ public class Principal {
 				
 			}
 
-			// Convertir el array en un string
-			String nombreAsignaturaS = new String(nombreAsignatura);
-
-			// Obtener la nota
-			nota = file.readFloat();
-
 			// Posicionarse en el siguiente alumno
 			posicion = posicion + LON_Notas;
 
 			// Salir del for cuando haya recorrido todos los bytes (Y cerrar el file)
-			if (file.getFilePointer() == file.length()) {
-				file.close();
+			if (posicion >= file.length()) {
 				break;
 			}
 
 		}
 		
+		// Cerrar el fichero
+		file.close();
 		
-		return 0;
+		return contadorAsignaturas;
 	}
 
 	// Método para listar las notas
@@ -294,7 +568,7 @@ public class Principal {
 				nota = file.readFloat();
 
 				// Imprimir el registro actual
-				System.out.printf("%7s %-20s %-20s %9s %11s %n", numAlumno, nombreS, localidadS, numAsignaturas, nota);
+				System.out.printf("%7s %-20s %-20s %9s %11.2f %n", numAlumno, nombreS, localidadS, numAsignaturas, nota);
 
 			}
 
