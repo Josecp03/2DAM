1109e5acf866c4587b0f192ddbf490c0b5185584
José Corrochano Pardo <118207818+Josecp03@users.noreply.github.com>
2025-02-06 21:12:04 +0100

    ejercicio javaHelp DI

---
+++ DESARROLLO_DE_INTERFACES/TEMA-7/Ejemplo-JavaHelp/javahelp/help/help_set.hs
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+ 
+<helpset version="1.0">
+        <title>Ejemplo ayuda JavaHelp</title>
+        <maps>
+                <!-- Página por defecto al mostrar la ayuda -->
+                <homeID>index</homeID>
+                <!-- Archivo de mapa -->
+                <mapref location="map_file.jhm"/>
+        </maps>
+
+        <!-- Vista: Tabla de Contenidos -->
+        <view>
+                <name>Tabla Contenidos</name>
+                <label>Tabla de contenidos</label>
+                <type>javax.help.TOCView</type>
+                <data>toc.xml</data>
+        </view>
+
+        <!-- Vista: Índice -->
+        <view>
+                <name>Indice</name>
+                <label>El indice</label>
+                <type>javax.help.IndexView</type>
+                <data>indice.xml</data>
+        </view>
+  
+        <!-- Vista: Buscar -->
+        <view>
+                <name>Buscar</name>
+                <label>Buscar</label>
+                <type>javax.help.SearchView</type>
+                <data engine="com.sun.java.help.search.DefaultSearchEngine">
+                  JavaHelpSearch
+                </data>
+        </view>
+  
+</helpset>
